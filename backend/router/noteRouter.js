9865eb12887eac1cc9e24126801041d5daf25a33
Ömer Meriç <kaynakca21@hotmail.com>
2024-10-19 21:29:55 +0300

    Update noteRouter.js

--- backend/router/noteRouter.js
+++ backend/router/noteRouter.js
@@ -1,16 +1,12 @@
 const express = require('express')
  const Note = require('../models/noteModel')
- const {createNote,getNotes,getNoteById,deleteNote, updateNote } = require('../controllers/noteController')
+ const {createNote,getNotes } = require('../controllers/noteController')
 const router = express.Router();
 
 router.get('/',getNotes)
 
-router.get('/:id',getNoteById)
 
 router.post('/',createNote)
 
-router.delete('/:id',deleteNote)
-
-router.patch('/',updateNote)
 
 module.exports = router;
