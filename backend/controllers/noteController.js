ae17757dfe435ebd8b0987c70965cc8cb9945790
Ömer Meriç <kaynakca21@hotmail.com>
2024-10-19 21:29:28 +0300

    Update noteController.js

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -22,43 +22,6 @@ const getNotes = async (req,res) =>{
     }
 }
 
-const getNoteById = async (req,res) =>{
-    const {id} = req.params;
-    try {
-        if(!mongoose.Types.ObjectId.isValid(id))   res.status(500).json({message:`${id} id'li not bulunamadı.}`})
-        const note = await Note.findById(id)
-        if(!note) return res.status(400).json({message:`${id} id'li not bulunamadı`})
-    
-            res.status(200).json({message:'Not başarıyla getirildi.',note})
-    } catch (error) {
-     res.status(500).json({message:`Error: ${error}`})
-        
-    }
-}
-
-const deleteNote = async (req,res) =>{
-    const {id} = req.params;
-
-    if(!mongoose.Types.ObjectId.isValid(id)) return  res.status(500).json({message:"ID geçersiz"})
-
-        const note = await Note.findOneAndDelete({_id:id})
-
-        if(!note) return res.status(400).json({message:"Not bulunamadı"})
-
-            res.status(200).json({message:"Not başarıyla silindi."})
-}
-
-const updateNote = async (req,res) => {
-    const {id} = req.params;
-    if(!mongoose.Types.ObjectId.isValid(id)) return  res.status(500).json({message:"ID geçersiz"})
-        const note = await Note.findOneAndUpdate({_id:id},{
-       ...req.body
-        },{new:true})
-        
-        if(!note) return res.status(400).json({message:"Not bulunamadı"})
-
-            res.status(200).json({message:"Not başarıyla güncellendi."})
-}
 
 
- module.exports = {createNote,getNotes,getNoteById,deleteNote, updateNote } ;
+ module.exports = {createNote,getNotes } ;
